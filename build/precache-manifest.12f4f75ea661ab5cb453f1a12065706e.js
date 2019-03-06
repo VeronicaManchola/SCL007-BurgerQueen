2a4ead469f81127f9f6c28b5e375e01d31866bf4
VeronicaManchola <veronica.manchola@hotmail.com>
2019-03-06 18:05:43 -0300

    deploy con firebase funcional

---
+++ build/precache-manifest.12f4f75ea661ab5cb453f1a12065706e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b028f20e397cd3cf4119",
+    "url": "/static/css/main.a474d66a.chunk.css"
+  },
+  {
+    "revision": "b028f20e397cd3cf4119",
+    "url": "/static/js/main.b028f20e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5eec6e4bcb5cbf0fe14547dbb539b69c",
+    "url": "/index.html"
+  }
+];
